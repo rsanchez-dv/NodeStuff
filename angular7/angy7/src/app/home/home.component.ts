e37e099cc46a8823a9b1f56ce5b79b08470fabb5
Roberto Sanchez <rsanchez92@live.com>
2018-10-22 20:28:33 -0700

    Button Toggle
    Completed Button toggle to change items in code

--- angular7/angy7/src/app/home/home.component.ts
+++ angular7/angy7/src/app/home/home.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  h1Style: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  firstClick() {
+    this.h1Style = !this.h1Style;
+    console.log('This was Clicked');
+  }
 }
