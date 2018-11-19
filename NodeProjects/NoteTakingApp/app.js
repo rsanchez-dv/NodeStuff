303fa53846a32a097a32e2dacee1b2598cd126ba
Roberto Sanchez <rsanchez92@live.com>
2018-11-18 18:50:34 -0800

    Base Node Project
    Set up of two files for node project

--- NodeProjects/NoteTakingApp/app.js
+++ NodeProjects/NoteTakingApp/app.js
@@ -1,14 +1,28 @@
-console.log('Starting App.js')
-
+// Used for files
 const fs = require('fs')
+// Used for Operating system uses
 const os = require('os')
+// Contains useful tools
+const _=require('lodash')
+// Used for parsing inputs
+const yargs = require('yargs')
+
+
 // Runs the file
 const notes = require('./notes')
+// Reads in arguements from command line
+const argv = yargs.argv;
+// Gets the command to run features
+var command = argv._[0]
 
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', 'Hello ' + user.username + "!"+"Your age is "+notes.age+"\n", function(err) {
-    if(err) {
-        console.log('Unable to write to file.')
-    }
-})
+if (command ==='add'){
+    notes.addNote(argv.title, argv.body)
+}else if(command === 'list'){
+    notes.getAll()
+} else if(command ==='remove'){
+    notes.remove(argv.title)
+} else if(command ==='read'){
+    notes.read(argv.title)
+} else {
+    console.log('Command not recognized.')
+}
--- NodeProjects/NoteTakingApp/notes.js
+++ NodeProjects/NoteTakingApp/notes.js
@@ -1,5 +1,20 @@
-console.log('Starting notes.js')
 
-module.exports.addNote = () => {
-    console.log('addnote')
+var addNote = (title, body) =>{
+    console.log('Adding note',title,body)
+}
+
+var getAll = () => {
+    console.log('Getting All notes')
+}
+var remove = (title) => {
+    console.log('Removing', title)
+}
+var read = (title) => {
+    console.log('Reading',title)
+}
+module.exports = {
+    addNote,
+    getAll,
+    remove,
+    read
 }
