7e12d1bab0c71a0e1739096bda41e23052e3b931
Roberto Sanchez <rsanchez92@live.com>
2018-11-19 21:13:18 -0800

    Completing Functions

--- NodeProjects/NoteTakingApp/app.js
+++ NodeProjects/NoteTakingApp/app.js
@@ -7,7 +7,6 @@ const _=require('lodash')
 // Used for parsing inputs
 const yargs = require('yargs')
 
-
 // Runs the file
 const notes = require('./notes')
 // Reads in arguements from command line
@@ -16,13 +15,32 @@ const argv = yargs.argv;
 var command = argv._[0]
 
 if (command ==='add'){
-    notes.addNote(argv.title, argv.body)
+    var note = notes.addNote(argv.title, argv.body)
+    if(note){
+        console.log('Note created')
+        console.log('-------------------------')
+        console.log('Title: ',`${note.title}`)
+        console.log('Body: ', `${note.body}`)
+    } else {
+        console.log('Note title already exists.')
+    }
 }else if(command === 'list'){
     notes.getAll()
 } else if(command ==='remove'){
-    notes.remove(argv.title)
+    var removed = notes.remove(argv.title)
+    var message = removed ? 'Note was removed.' : 'Note not found.'
+    console.log(message)
+
 } else if(command ==='read'){
-    notes.read(argv.title)
+    var note = notes.read(argv.title)
+    if(note) {
+        console.log('Note Found!')
+        console.log('-------------------------')
+        console.log('Title: ',`${note.title}`)
+        console.log('Body: ', `${note.body}`)
+    }else {
+        console.log('Note not found.')
+    }
 } else {
     console.log('Command not recognized.')
 }
--- NodeProjects/NoteTakingApp/notes.js
+++ NodeProjects/NoteTakingApp/notes.js
@@ -1,16 +1,55 @@
+const fs = require('fs')
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json')
+        return JSON.parse(notesString)
+    } catch(e){
+        console.log("Could not find existing file. Creating new.")
+        return []
+    }
+}
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json',  JSON.stringify(notes))
+
+}
 
 var addNote = (title, body) =>{
-    console.log('Adding note',title,body)
+    var notes = fetchNotes()
+    var note = {
+        title,
+        body
+    }
+    // Moves any matches note.title with title and moves it to the duplicateNotes
+    var duplicateNotes = notes.filter((note) => note.title === title)
+
+    if (duplicateNotes.length === 0){
+        notes.push(note)
+        saveNotes(notes)
+        return note
+    }
 }
 
 var getAll = () => {
     console.log('Getting All notes')
 }
 var remove = (title) => {
-    console.log('Removing', title)
+    var notes = fetchNotes()
+    // Moves all the titles that do not match title and moves them to filteredNotes.
+    var filteredNotes = notes.filter((note) => note.title !== title)
+    saveNotes(filteredNotes)
+    return notes.length !== filteredNotes.length
 }
 var read = (title) => {
-    console.log('Reading',title)
+    var notes = fetchNotes()
+    var filteredNotes = notes.filter((note) => note.title === title)
+    return filteredNotes[0]
+}
+var logNote = (Note) => {
+    console.log('---')
+    console.log(`Title" ${note.title}`)
+    console.log(`Body" ${note.body}`)
 }
 module.exports = {
     addNote,
