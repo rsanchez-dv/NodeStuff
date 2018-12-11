7a1ef75f5a70c4135022a16a1317914f7f72f051
Roberto Sanchez <rsanchez92@live.com>
2018-12-11 11:58:27 -0800

    Completed private route

--- NodeProjects/TodoApp/server/server.js
+++ NodeProjects/TodoApp/server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -27,6 +28,9 @@ app.post('/users', (req, res) => {
     })
   });
   
+  app.get('/users/me', authenticate,(req,res)=>{
+    res.send(req.user);
+  });
 
 app.post('/todos',(req,res)=>{
     var todo = new Todo({
