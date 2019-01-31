0a0e8062776ab55abd9a895f8b6245ae2ea20656
Roberto Sanchez <rsanchez92@live.com>
2019-01-31 15:46:17 -0800

    Testing Login

--- NodeProjects/TodoApp/server/tests/server.test.js
+++ NodeProjects/TodoApp/server/tests/server.test.js
@@ -227,13 +227,11 @@ describe('POST /users', () => {
           return done(err);
         }
 
-        User.findOne({
-          email
-        }).then((user) => {
+        User.findOne({email}).then((user) => {
           expect(user).not.toBeFalsy();
           expect(user.password).not.toBe(password);
           done();
-        });
+        }).catch((e)=>{done(e);})
       });
   });
 
@@ -257,5 +255,37 @@ describe('POST /users', () => {
       })
       .expect(400)
       .end(done);
+  });
+});
+
+// Testing Login
+describe('POST /users/login',()=>{
+  it('should login user and return auth token', (done)=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.header['x-auth']).toExist();
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      User.findById(user[1]._id).then((user)=>{
+        expect(user.tokens[0].toInclude({
+          access:'auth',
+          token: res.headers['x-auth']
+        }));
+        done();
+      }).catch((e)=>{done(e);})
+    })
+  });
+
+  it('should reject invalid login',(done)=>{
+
   });
 });
