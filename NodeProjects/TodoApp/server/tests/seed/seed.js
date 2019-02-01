585bfd9da76b2524f71d1e4b41281331daf607ca
Roberto Sanchez <rsanchez92@live.com>
2019-01-31 19:01:13 -0800

    Completed Project environmental Variables

--- NodeProjects/TodoApp/server/tests/seed/seed.js
+++ NodeProjects/TodoApp/server/tests/seed/seed.js
@@ -14,7 +14,7 @@ const users = [
         password: 'userpassword',
         tokens: [{
             access: 'auth',
-            token: jwt.sign({_id: userOneId,access:'auth'},'asdf').toString()
+            token: jwt.sign({_id: userOneId,access:'auth'},process.env.JWT_SECRET).toString()
             }]
     },
     {
@@ -23,7 +23,7 @@ const users = [
         password: 'userpassword123',
         tokens: [{
             access: 'auth',
-            token: jwt.sign({_id: userTwoId,access:'auth'},'asdf').toString()
+            token: jwt.sign({_id: userTwoId,access:'auth'},process.env.JWT_SECRET).toString()
             }]
     }
 ];
