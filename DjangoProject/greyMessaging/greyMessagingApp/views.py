1d1fb53a04c1174c36d5c09596dc2f633fa116e5
Roberto Sanchez <rsanchez92@live.com>
2019-02-14 14:54:22 -0800

    Removal

--- DjangoProject/greyMessaging/greyMessagingApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Topic,Webpage,AccessRecord
-# Create your views here.
-
-def index(request):
-    webpages_list = AccessRecord.objects.order_by('date')
-    date_dict = {'access_records': webpages_list}
-    return render(request,'greyMessagingApp/index.html',context=date_dict)
